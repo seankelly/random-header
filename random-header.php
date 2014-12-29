4a9fe1dbb686b25ff2d0f17b39580e86c8999091
Sean Kelly <code@katron.org>
2014-12-29 13:51:50 -0600

    Add basic framework to add hooks where needed to get plugin running

--- random-header.php
+++ random-header.php
@@ -8,3 +8,20 @@
  */
 
 defined('ABSPATH') or die("This file must be used with WordPress.");
+
+class RandomHeader {
+	public static function setup() {
+		add_action('init', array(__CLASS__, 'init'));
+	}
+
+	public static function init() {
+		if (current_theme_supports('custom-header')) {
+			add_action('theme_mod_header_image', array(__CLASS__, 'theme_mod_header_image'));
+		}
+	}
+
+	public static function theme_mod_header_image($header_url) {
+	}
+}
+
+RandomHeader::setup();
