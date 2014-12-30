5dfeca7c6c7c01af4583affe6585b6d402b2a22a
Sean Kelly <code@katron.org>
2014-12-29 20:36:22 -0600

    Query images that have titles that match a certain pattern
    Switched to using an instantiation of the class. The
    theme_mod_header_image action is run six times in the Twenty Fourteen
    theme. It doesn't make sense to run a potentially expensive, impossible
    to cache query six times so using an instantiation to allow caching the
    image after the first query.

--- random-header.php
+++ random-header.php
@@ -10,18 +10,47 @@
 defined('ABSPATH') or die("This file must be used with WordPress.");
 
 class RandomHeader {
-	public static function setup() {
-		add_action('init', array(__CLASS__, 'init'));
+	public function __construct() {
+		add_action('init', array($this, 'init'));
 	}
 
-	public static function init() {
+	public function init() {
 		if (current_theme_supports('custom-header')) {
-			add_action('theme_mod_header_image', array(__CLASS__, 'theme_mod_header_image'));
+			add_action('theme_mod_header_image', array($this, 'theme_mod_header_image'));
 		}
 	}
 
-	public static function theme_mod_header_image($header_url) {
+	public function theme_mod_header_image($header_url) {
+		add_filter('posts_where', array($this, media_where_filter), 10, 2);
+		$images = new WP_Query(array(
+			'post_type' => 'attachment',
+			'post_status' => 'inherit',
+			'post_mime_type' => 'image',
+			'orderby' => 'rand',
+			'posts_per_page' => 10,
+			'theme_mod_header_image_title' => 'wgom banner',
+		));
+		remove_filter('posts_where', array($this, media_where_filter), 10, 2);
+
+		echo "<!-- {$images->request} -->";
+
+		foreach ($images->posts as $image) {
+			$image_url = wp_get_attachment_url($image->ID);
+			echo "<!--  {$image->post_title} : $image_url -->\n";
+		}
+	}
+
+	public function media_where_filter($where, &$query) {
+		global $wpdb;
+		if ($title = $query->get('theme_mod_header_image_title')) {
+			$where .= ' AND '
+				. $wpdb->posts
+				. '.post_title LIKE \''
+				. esc_sql($wpdb->esc_like($title))
+				. '%\'';
+		}
+		return $where;
 	}
 }
 
-RandomHeader::setup();
+new RandomHeader();
